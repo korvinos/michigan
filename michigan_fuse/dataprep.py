dd835545aea44212ce350933bab5414949a540dd
korvinos <moiseev.artem@gmail.com>
2017-04-07 13:07:27 +0300

    s2_make_granules function created, new wavelengths sets added, refactoring

--- michigan_fuse/dataprep.py
+++ michigan_fuse/dataprep.py
@@ -1,5 +1,6 @@
 from nansat import Nansat, Domain
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 import re
 import glob
@@ -9,8 +10,13 @@ class Data:
     # Different bands sets for each sensors
     wavelengths = {
         'modis': {
-            'full': [412, 443, 488, 531, 547, 555, 645, 667, 678],
-            '1x1km_bands': [412, 443, 488, 531, 645, 678]
+            'full': [412, 443, 488, 531, 547, 555, 645, 667, 678],  # All MODISa channels
+            '1x1km_bands': [412, 443, 488, 531, 645, 678],  # Only 1x1 km spatial resolution bands
+            'blue_off': [443, 469, 488, 531, 547, 555, 645, 667, 678],  # Without 412 nm
+            'red_off': [412, 443, 469, 488, 531, 547, 555, 645, 667],  # Without 678 nm
+            'blue_and_red_off': [443, 469, 488, 531, 547, 555, 645, 667],  # Without 412 nm and 678 nm
+            'red_off_full': [412, 443, 469, 488, 531, 547, 555],
+            '1x1km_bands_2': [443, 488, 531, 645, 678]  # Only 1x1 km spatial resolution bands
         },
 
         'sentinel2': [443, 490, 560, 665, 705, 740, 783, 842, 945, 1375, 1610, 2190]
@@ -33,21 +39,21 @@ class Data:
     domain = Domain('+proj=latlong +datum=WGS84 +ellps=WGS84 +no_defs', '-lle -86.3 44.6 -85.2 45.3 -ts %s %s'
                     % (x_resolution, y_resolution))
 
-    BANDS = {
-        '01': {'wavelength': 443, 'resolution': 60},
-        '02': {'wavelength': 490, 'resolution': 10},
-        '03': {'wavelength': 560, 'resolution': 10},
-        '04': {'wavelength': 665, 'resolution': 10},
-        '05': {'wavelength': 705, 'resolution': 20},
-        '06': {'wavelength': 740, 'resolution': 20},
-        '07': {'wavelength': 783, 'resolution': 20},
-        '08': {'wavelength': 842, 'resolution': 10},
-        #    '8A':{ 'wavelength': 865, 'resolution': 20 },
-        '09': {'wavelength': 945, 'resolution': 60},
-        '10': {'wavelength': 1375, 'resolution': 60},
-        '11': {'wavelength': 1610, 'resolution': 20},
-        '12': {'wavelength': 2190, 'resolution': 20}
-    }
+    # BANDS = {
+    #     '01': {'wavelength': 443, 'resolution': 60},
+    #     '02': {'wavelength': 490, 'resolution': 10},
+    #     '03': {'wavelength': 560, 'resolution': 10},
+    #     '04': {'wavelength': 665, 'resolution': 10},
+    #     '05': {'wavelength': 705, 'resolution': 20},
+    #     '06': {'wavelength': 740, 'resolution': 20},
+    #     '07': {'wavelength': 783, 'resolution': 20},
+    #     '08': {'wavelength': 842, 'resolution': 10},
+    #     #    '8A':{ 'wavelength': 865, 'resolution': 20 },
+    #     '09': {'wavelength': 945, 'resolution': 60},
+    #     '10': {'wavelength': 1375, 'resolution': 60},
+    #     '11': {'wavelength': 1610, 'resolution': 20},
+    #     '12': {'wavelength': 2190, 'resolution': 20}
+    # }
 
     # <granules> is list of granules which covers Sandy Bear Dunes region.
     # To get more information about granules see:
@@ -105,24 +111,32 @@ class Data:
         return n_export
 
     def stich(self, domain, bands, gdirs):
+        """
+        :param domain: 
+        :param bands: list
+        :param gdirs: list, list of directories paths  
+        :return: 
+        """
+        # Create base nansat object which domain covers all four <granules>
+        n_obj = Nansat(domain=domain)
         for band in bands:
             print('Band:', band)
             # Generate 2d array which will have shape like <d> and filled by nan
-            bandArray = np.zeros(domain.shape()) + np.nan
+            band_arr = np.zeros(domain.shape()) + np.nan
             for gdir in gdirs:
                 bfile = glob.glob(os.path.join(gdir, 'IMG_DATA', '*_B%s.jp2' % band))[0]
                 n = Nansat(bfile)
                 # Reprojection of data according to domain; eResampleAlg 1 is Bilinear
-                n.reproject(d, eResampleAlg=1, addmask=False)
+                n.reproject(domain, eResampleAlg=1, addmask=False)
                 # Get data array from nansat object
                 bdata = n[1]
-                bandArray[bdata > 0] = bdata[bdata > 0]
+                band_arr[bdata > 0] = bdata[bdata > 0]
 
-            n_obj.add_band(bandArray, parameters={'name': 'Rrs_%s' % BANDS[band]['wavelength']})
+            n_obj.add_band(band_arr, parameters={'name': 'Rrs_%s' % band})
 
         return n_obj
 
-    def s2_downscale(self):
+    def s2_downscale(self, save_path='./'):
 
         if re.match(r'S2A', self.s_file):
             raise IOError
@@ -135,6 +149,7 @@ class Data:
         # Lists for accumulation of lon/lat values from each granule
         lons = []
         lats = []
+
         for gdir in gdirs:
             # Find absolute path to relevant granule
             b0file = glob.glob(os.path.join(gdir, 'IMG_DATA', '*_B01.jp2'))[0]
@@ -151,9 +166,87 @@ class Data:
             min(lons), min(lats), max(lons), max(lats)))
         print('Domain created')
 
-        # Create base nansat object which domain covers all four <granules>
-        bands = self.wavelengths['sentinel2']
-        n_obj = self.stich(d, bands, gdirs)
+        n_obj = self.stich(d, self.wavelengths['sentinel2'], gdirs)
         n_obj.reproject(self.domain)
-        export_name = os.path.split(self.s_file)[1] + '_full.nc'
-        n_obj.export("output/%s" % export_name)
+        export_name = os.path.split(self.s_file)[1] + '_reprojected.nc'
+        n_obj.export(os.path.join(save_path, export_name))
+
+    def s2_make_granules(self, save_path='./'):
+
+        if re.match(r'S2A', self.s_file):
+            raise IOError
+
+        # Get list of granules
+        # What is granules: https://sentinels.copernicus.eu/web/sentinel/user-guides/sentinel-2-msi/product-types
+        # Add path to sentinel image <ifile>
+
+        gdirs = sorted(glob.glob(os.path.join(self.s_file, 'GRANULE', '*')))
+
+        lons = []  # List of longitudes for each granule
+        lonVec = []  # ???
+        lats = []  # List of latitudes for each granule
+        latVec = []  # ???
+        labels = []  # List on names for each granule
+        # Part I: Generation of domain
+        # For each granule in granules list / gdirs
+        for gdir in gdirs:
+            # Get B01.jp2 image (granule) path
+            # About jp2: https://ru.wikipedia.org/wiki/JPEG_2000
+            b01file = glob.glob(os.path.join(gdir, 'IMG_DATA', '*_B01.jp2'))[0]
+
+            # Get granule image by nansat
+            n = Nansat(b01file)
+            # Get max/min lat and long of granule
+            lon, lat = n.get_border()
+            ## print lon, lat
+            lons += list(lon)
+            lats += list(lat)
+
+            # For what this two rows ???
+            lonVec.append(lon)
+            latVec.append(lat)
+            # Add name/label of granule to list
+            labels.append(b01file.split('_')[-2][1:])
+
+        # Create Domain which is based on max and min lat and long from all (!!!) granules
+        # 1000 x 1000 is resolution of MODIS-a img ?
+        d = Domain(n.vrt.get_projection(), '-lle %f %f %f %f -tr 1000 1000' % (
+            min(lons), min(lats), max(lons), max(lats)))
+
+        ## print(d)
+
+        # Create name for export of img which all granules
+        mapfile = os.path.split(self.s_file)[1] + '_map.png'
+        ## print(mapfile)
+        d.write_map(mapfile, resolution='h',
+                    lonVec=lonVec, latVec=latVec,
+                    lonBorder=1, latBorder=1,
+                    dpi=150,
+                    labels=labels)
+
+        # Part II: Reprojection of granules
+        # Generate 2d array which will have shape like #d annd fill by nan
+        s2array = np.zeros(d.shape()) + np.nan
+        # For each granule in granules list / gdirs
+        for gdir in gdirs:
+            # Get B01.jp2 image (granule) path
+            b01file = glob.glob(os.path.join(gdir, 'IMG_DATA', '*_B01.jp2'))[0]
+            ## print b01file
+            n = Nansat(b01file)
+            # Reprojection of granule according #d, 0 is nearest neighbour
+            n.reproject(d, eResampleAlg=0)
+            # Get data array from nansat obj
+            b1 = n[1]
+            # All areas in #s2array array will filled by vales from granule
+            # As result, after loop we will get one object which contain data from all granules
+            s2array[b1 > 0] = b1[b1 > 0]
+
+        # Create nansat obj according #d which contain watter mask
+        wm = Nansat(domain=d).watermask()[1]
+        # Why wm == 2 ?? From documentation: Create numpy array with watermask (water=1, land=0)
+        s2array[wm == 2] = 0
+
+        # Create name of file
+        qlfile = os.path.split(self.s_file)[1] + '_ql.png'
+        # Save on disk full generated image: #s2array
+        plt.imsave(os.path.join(save_path, qlfile), s2array, vmin=1050, vmax=1400)
